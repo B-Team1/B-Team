138f1ba08d35807660d4014522e393b0e791cfa8
Tobias <tobias.thuering1@students.fhnw.ch>
2015-11-13 10:06:20 +0100

    Installation Server

--- KingOfTokyoBTeamServer/src/ch/fhnw/itprojekt/bteam/server/Server.java
+++ KingOfTokyoBTeamServer/src/ch/fhnw/itprojekt/bteam/server/Server.java
@@ -4,29 +4,46 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Server {
- ResultSet rs;
- PreparedStatement ps;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+	private ServerView view;
+	private ServerController controller;
+	private ConnectionModel connectionModel;
+
+	public static void main(String[] args) {
+        launch(args);
+    }
  
-	public static void main(String [] args){
-		try {
-			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-			Connection con = DriverManager.getConnection("jdbc:odbc:BTeam","","");
-			System.out.println("Connected");
-			//Statement stm = con.createStatement();
-			//String sqlQuerry = "Select * from User";
-			
-			//ResultSet rs = stm.executeQuery(sqlQuerry);
-			
-			//while(rs.next()){
-			//	System.out.println(rs.getString("NName"));
-			//}
-			
-			
-			
-			} catch (Exception e){
-				System.out.println (e.getMessage());
-		}
-	}
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        // Part of the GUI will contain log output from our own handler
+        TextAreaHandler textAreaHandler = new TextAreaHandler();
+        textAreaHandler.setLevel(Level.INFO);
+        Logger defaultLogger = Logger.getLogger("");
+        defaultLogger.addHandler(textAreaHandler);
+        
+        // Initialize the GUI
+        connectionModel = new ConnectionModel();
+        view = new ServerView(primaryStage, connectionModel, textAreaHandler.getTextArea());
+        controller = new ServerController(connectionModel, view);
+
+        // Display the GUI after all initialization is complete
+        view.start();
+    }
+
+    /**
+     * The stop method is the opposite of the start method. It provides an
+     * opportunity to close down the program gracefully, when the program has
+     * been closed.
+     */
+    @Override
+    public void stop() {
+        if (view != null)
+            view.stop();
+    }
 }
---
+++ KingOfTokyoBTeamServer/src/ch/fhnw/itprojekt/bteam/server/ServerThreadForClient.java
@@ -0,0 +1,64 @@
+package ch.fhnw.itprojekt.bteam.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+public class ServerThreadForClient extends Thread {
+    private final Logger logger = Logger.getLogger("");
+    private Socket clientSocket;
+
+    public ServerThreadForClient(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    /**
+     * Process messages until the client says "Goodbye"
+     */
+    @Override
+    public void run() {
+    	Message.MessageType lastMessageType = null;
+        
+        logger.info("Request from client " + clientSocket.getInetAddress().toString()
+                + " for server " + clientSocket.getLocalAddress().toString());
+
+        try {
+            while (lastMessageType != Message.MessageType.Goodbye) {
+				// Read a message from the client
+				Message msgIn = Message.receive(clientSocket);
+				lastMessageType = msgIn.getType();
+				Message msgOut = processMessage(msgIn);				
+				msgOut.send(clientSocket);
+            }
+        } catch (Exception e) {
+            logger.severe(e.toString());
+        } finally {
+            try { if (clientSocket != null) clientSocket.close(); } catch (IOException e) {}
+        }
+    }
+    
+    private Message processMessage(Message msgIn) {
+		logger.info("Message received from client: "+ msgIn.toString());						
+
+		Message msgOut = null;
+		switch (msgIn.getType()) {
+		case Hello:
+			msgOut = new Message(Message.MessageType.Hello);
+			break;
+		case NewClient:
+			msgOut = new Message(Message.MessageType.NewClientAccepted);
+			msgOut.setName(msgIn.getName());
+			break;
+		case Goodbye:
+			msgOut = new Message(Message.MessageType.Goodbye);
+			break;
+		default:
+			msgOut = new Message(Message.MessageType.Error);
+		}
+    	logger.info("Message answered: " + msgOut.toString());
+    	return msgOut;
+    }
+}
