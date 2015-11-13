e67466a0c4f0f2f826078985ca391c0eb55c6dea
Luzian <Luzian@10.207.4.102>
2015-11-13 09:09:42 +0100

    Datebank connection

--- KingOfTokyoBTeamServer/src/ch/fhnw/itprojekt/bteam/server/Server.java
+++ KingOfTokyoBTeamServer/src/ch/fhnw/itprojekt/bteam/server/Server.java
@@ -1,5 +1,32 @@
 package ch.fhnw.itprojekt.bteam.server;
 
-public class Server {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class Server {
+ ResultSet rs;
+ PreparedStatement ps;
+ 
+	public static void main(String [] args){
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			Connection con = DriverManager.getConnection("jdbc:odbc:BTeam","","");
+			System.out.println("Connected");
+			//Statement stm = con.createStatement();
+			//String sqlQuerry = "Select * from User";
+			
+			//ResultSet rs = stm.executeQuery(sqlQuerry);
+			
+			//while(rs.next()){
+			//	System.out.println(rs.getString("NName"));
+			//}
+			
+			
+			
+			} catch (Exception e){
+				System.out.println (e.getMessage());
+		}
+	}
 }
