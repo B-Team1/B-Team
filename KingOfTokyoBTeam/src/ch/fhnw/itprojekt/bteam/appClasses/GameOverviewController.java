c43970b77cc010043276d6f82da416427e2c16cb
B-Team1 <luzian.luethi@students.fhnw.ch>
2015-11-18 15:32:39 +0100

    New Password Field

---
+++ KingOfTokyoBTeam/src/ch/fhnw/itprojekt/bteam/appClasses/GameOverviewController.java
@@ -0,0 +1,31 @@
+package ch.fhnw.itprojekt.bteam.appClasses;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class GameOverviewController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+	
+	@FXML
+	public void handleStartGame(ActionEvent event) {
+	
+	}
+	
+	@FXML
+	public void handleNewGame(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	public void handleCloseGame(ActionEvent event) {
+		
+	}
+	
+}
