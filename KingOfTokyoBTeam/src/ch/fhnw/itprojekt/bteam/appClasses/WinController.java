aba780182a4059aa3cffa25c0ba476d7adce88cd
B-Team1 <luzian.luethi@students.fhnw.ch>
2015-11-25 09:50:08 +0100

    NewGame Window

---
+++ KingOfTokyoBTeam/src/ch/fhnw/itprojekt/bteam/appClasses/WinController.java
@@ -0,0 +1,33 @@
+package ch.fhnw.itprojekt.bteam.appClasses;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class WinController implements Initializable {
+
+	/**
+	 * Methode initialisiert die Komponenten
+	 * @author Marco
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+	
+	/**
+	 * Methode schliesst die Fenster und kehrt zur Spiel Übersicht zurück
+	 * @author Marco
+	 */
+	public void handleQuitGame(ActionEvent event) {
+		MenuModel model = new MenuModel();
+		model.start(new Stage());
+		Node node= (Node)event.getSource();
+		Stage stage = (Stage) node.getScene().getWindow();
+		stage.close();
+	}
+	
+}
