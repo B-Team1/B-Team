5f2df9f0f4bb3da0a1b6ab39f476e4df6b837184
B-Team1 <luzian.luethi@students.fhnw.ch>
2015-12-17 15:40:24 +0100

    tokyoscheiss

--- KingOfTokyoBTeam/src/ch/fhnw/itprojekt/bteam/appClasses/GameModel.java
+++ KingOfTokyoBTeam/src/ch/fhnw/itprojekt/bteam/appClasses/GameModel.java
@@ -20,7 +20,7 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class GameModel extends Application {
-	private int lifePoints, energyPoints, honorPoints, honorPointsWin = 25;
+	private int lifePoints, energyPoints, honorPoints, honorPointsWin;
 	static int cardCost = 3;
 	private boolean inTokyo = false;
 	static public ArrayList<Card> cardList = new ArrayList<Card>();
@@ -394,6 +394,9 @@ public class GameModel extends Application {
 	  */
 	public void checkLoser() {
 		if (players.get(myPosition).getLifePoints() <= 0) {
+			if (players.get(myPosition).isInTokyo()) {
+				sendTokyoChange(true);
+			}
 			startLoser(new Stage());
 		}
 		for (int i = 0; i < players.size(); i++) {
@@ -419,10 +422,8 @@ public class GameModel extends Application {
 				}
 			}
 		if (famePointsWin) {
-			for (int i = 0; i < players.size(); i++) {
-				if (players.get(myPosition).getHonorPoints() >= honorPointsWin) {
-						win = true;
-				}
+			if (players.get(myPosition).getHonorPoints() >= honorPointsWin) {
+				win = true;
 			}
 		}	
 		if (win) {
